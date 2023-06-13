aec161a56d1b67f342c75dc4421d4949d4f8d9f8
Timo Hofmeijer <t@hofmeijer.me>
2023-06-13 10:13:37 +0200

    chore: create typographic hierarchy to home intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,12 @@ export default function Home() {
         Welkom op website van de wielrengroep van AVTriathlon! Deze website is in het leven geroepen
         om de afspraken en signalen van onze groepsritten helder te kunnen communiceren.
       </p> */}
-        <p className="mb-4">
+        <p className="mb-4 text-lg">
           Op deze site vind je een overzicht van de afspraken en signalen die we hanteren tijdens de
           wielren groepsritten van AVTriathlon. Omdat het net iets te veel informatie is om voor een
           rit uit te leggen, hebben we het hier netjes op een rijtje gezet.
         </p>
+        <h2 className="text-2xl font-bold pt-8 pb-4">Wat willen we hier mee bereiken</h2>
         <p className="mb-4">
           Wanneer je in een groep rijdt ben je je minder bewust van je omgeving doordat je minder
           ziet en vaak ook met mensen aan praten bent. Dat compenseren we door op een voorspelbare
@@ -42,14 +43,26 @@ export default function Home() {
 
         <section>
           <h2 className="text-2xl font-bold pt-8 pb-4">Formaties</h2>
-          {/* <p>Of lees je dit voor het eerst 😀</p> */}
+          <p>
+            De voorste renners van een groep zullen een stuk harder moeten trappen omdat ze ‘in de
+            wind rijden’. Afhankelijk van waar de wind vandaan komt kan dit meer dan 30% schelen. Om
+            het kopwerk te verdelen roteren we elke paar minuten.
+          </p>
           <LinkButton href="/draaien">Hoe wordt er gedraaid</LinkButton>
+          <p>
+            De voorste renners van een groep zullen een stuk harder moeten trappen omdat ze ‘in de
+            wind rijden’. Afhankelijk van waar de wind vandaan komt kan dit meer dan 30% schelen. Om
+            het kopwerk te verdelen roteren we elke paar minuten.
+          </p>
           <LinkButton href="/ritsen">Wanneer en hoe wij ritsen</LinkButton>
         </section>
 
         <section>
           <h2 className="text-2xl font-bold pt-8 pb-4">Ben je nieuw?</h2>
-          <p>Of lees je dit voor het eerst 😀.</p>
+          <p>
+            (Of lees je dit voor het eerst 😀) Leuk dat je mee komt rijden! Lees het volgende even
+            door, dan heb je alvast een idee wat we van elkaar mogen verwachten.
+          </p>
           <LinkButton href="/nieuwe-deelnemers">Lees dit even door</LinkButton>
         </section>
 
