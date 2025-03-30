5b2595dd6a2acdbe6f03ad12f0844c5138cf4395
Timo Hofmeijer <t@hofmeijer.me>
2025-03-30 19:00:14 +0200

    temp fix for page animation error

--- src/components/PageAnimation.tsx
+++ src/components/PageAnimation.tsx
@@ -8,18 +8,26 @@ export const PageAnimation: React.FC<{
   direction?: 'left' | 'right'
   className?: string
 }> = ({ children, direction = 'right', className }) => {
-  const x = direction === 'left' ? -200 : 200
-  const path = usePathname()
-  return (
-    <motion.div
-      key={path}
-      initial={{ x, opacity: 0 }}
-      animate={{ z: 99, x: 0, opacity: 1 }}
-      exit={{ x, opacity: 0 }}
-      transition={{ duration: 0.4 }}
-      className={cn(className, 'min-h-screen w-full')}
-    >
-      {children}
-    </motion.div>
-  )
+  return <div className={cn(className, 'min-h-screen w-full')}>{children}</div>
 }
+
+// export const OFFPageAnimation: React.FC<{
+//   children: React.ReactNode
+//   direction?: 'left' | 'right'
+//   className?: string
+// }> = ({ children, direction = 'right', className }) => {
+//   const x = direction === 'left' ? -200 : 200
+//   const path = usePathname()
+//   return (
+//     <motion.div
+//       key={path}
+//       initial={{ x, opacity: 0 }}
+//       animate={{ z: 99, x: 0, opacity: 1 }}
+//       exit={{ x, opacity: 0 }}
+//       transition={{ duration: 0.4 }}
+//       className={cn(className, 'min-h-screen w-full')}
+//     >
+//       {children}
+//     </motion.div>
+//   )
+// }
